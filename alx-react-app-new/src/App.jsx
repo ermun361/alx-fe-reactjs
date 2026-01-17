5c681c6300733845d2b428e142496c3983b5b229
Eric munyi <ericmunyi361@gmail.com>
2026-01-17 12:11:29 +0300

    Add Counter component with state and hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,7 +3,9 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';   
+import Counter from './components/Counter';
 import './App.css';
+
 
 
 function App() {
@@ -22,6 +24,7 @@ function App() {
           age="25" 
           bio="Loves hiking and photography" />
       <MainContent />
+      <Counter />
       <Footer />
     </div>
   );
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main style={{ backgroundColor: '#f4f4f4', padding: '20px', textAlign: 'center' }}>
+    <main style={{ backgroundColor: '#3b3434', padding: '20px', textAlign: 'center' }}>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
