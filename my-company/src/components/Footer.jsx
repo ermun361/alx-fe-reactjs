9da600615445f9eae7bfb7a17872ca2f14b23ea8
Eric munyi <ericmunyi361@gmail.com>
2026-01-18 04:59:58 +0300

    Add my-company project with React Router and components

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer style={{ backgroundColor: '#f1f1f1', padding: '10px', textAlign: 'center', position: 'fixed', bottom: '0', width: '100%' }}>
+            <p>© 2026 Eric Munyi. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
