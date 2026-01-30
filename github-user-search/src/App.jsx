cc799d65892c5ddd86a79c1cd89e3d70759827be
Eric Munyi <munyieric7@gmail.com>
2026-01-30 20:16:27 +0300

    Initialize GitHub User Search project in alx-fe-reactjs

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      {/* Search components will go here */}
+    </div>
+  )
+}
+
+export default App
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,1 @@
+const apiKey = import.meta.env.VITE_GITHUB_API_KEY;
